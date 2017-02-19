e3283752b61c98e5c77dd56c8ff0fade8aa79059
Albert I <krascgq@outlook.co.id>
2017-02-19 23:33:43 +0800

    mm: Import and forward-ported UKSM 0.1.2.3
    This version of UKSM has been forward-ported for usage with Linux 3.10.105, mostly include changes from ksm.c with Linux 3.10.46+.
    
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- fs/exec.c
+++ fs/exec.c
@@ -55,6 +55,7 @@
 #include <linux/pipe_fs_i.h>
 #include <linux/oom.h>
 #include <linux/compat.h>
+#include <linux/ksm.h>
 
 #include <asm/uaccess.h>
 #include <asm/mmu_context.h>
@@ -1003,7 +1004,7 @@ void setup_new_exec(struct linux_binprm * bprm)
 
 
 	current->self_exec_id++;
-			
+
 	flush_signal_handlers(current, 0);
 }
 EXPORT_SYMBOL(setup_new_exec);
